97371c02df2420ffc748d34902c5deae948cd3c2
faseeh <faseeh.shahzad@devsinc.com>
2021-09-11 23:29:00 +0500

    seeds.rb updated with demo data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'Ali',email: 'ali@gmail.com')
+micro = Micropost.create(content: 'This is a test Micropost for Ali', user_id: 1)
