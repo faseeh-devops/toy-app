aa20f7c023cf732d518ca28726046e8561a2481b
faseeh <faseeh.shahzad@devsinc.com>
2021-09-12 00:25:49 +0500

    microposts_controller.rb updated.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
 class MicropostsController < ApplicationController
-  before_action :set_micropost, only: %i[ show edit update destroy ]
+  before_action :set_micropost, only: %i[show edit update destroy]
 
   # GET /microposts or /microposts.json
   def index
-    @microposts = Micropost.all
+    @user = User.find(params[:user_id])
+    @microposts = @user.microposts.find(:id)
   end
 
   # GET /microposts/1 or /microposts/1.json
-  def show
-  end
+  def show; end
 
   # GET /microposts/new
   def new
@@ -16,54 +17,64 @@ class MicropostsController < ApplicationController
   end
 
   # GET /microposts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /microposts or /microposts.json
+  # @return [Object]
   def create
-    @micropost = Micropost.new(micropost_params)
-
-    respond_to do |format|
-      if @micropost.save
-        format.html { redirect_to @micropost, notice: "Micropost was successfully created." }
-        format.json { render :show, status: :created, location: @micropost }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /microposts/1 or /microposts/1.json
-  def update
-    respond_to do |format|
-      if @micropost.update(micropost_params)
-        format.html { redirect_to @micropost, notice: "Micropost was successfully updated." }
-        format.json { render :show, status: :ok, location: @micropost }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
+    # @post = Post.find(params[:post_id])
+    # @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    @user = User.find(params[:user_id])
+    @micropost = @user.microposts.create(params[:micropost].permit(:content, :id))
+    # redirect_to user_micropost_url
+    # respond_to do |format|
+    #   if @micropost.save
+    #     format.html { redirect_to micr, notice: 'Micropost was successfully created.' }
+    #     format.json { render :show, status: :created, location: @micropost }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @micropost.errors, status: :unprocessable_entity }
+    #   end
   end
 
   # DELETE /microposts/1 or /microposts/1.json
   def destroy
+    @user = User.find(params[:user_id])
+    @micropost = @user.microposts.find(params[:id])
     @micropost.destroy
+
     respond_to do |format|
-      format.html { redirect_to microposts_url, notice: "Micropost was successfully destroyed." }
+      format.html { redirect_to user_microposts_url, notice: 'Micropost was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
-    end
-end
+  end
+
+  # # PATCH/PUT /microposts/1 or /microposts/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @micropost.update(micropost_params)
+  #       format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @micropost }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @micropost.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+ 
+
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_micropost
+  #     @micropost = Micropost.find(params[:id])
+  #   end
+  #
+  #   # Only allow a list of trusted parameters through.
+  #   def micropost_params
+  #     params.require(:micropost).permit(:content, :user_id)
+  #   end
+
